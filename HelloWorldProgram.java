e79e2a7a71831e39ce877d8708248eac4de93594
meghana_777 <17wh1a0504@bvrithyderabad.edu.in>
2020-01-26 19:09:33 +0530

    Added Repuplic Day wishes

--- HelloWorldProgram.java
+++ HelloWorldProgram.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class HelloWorldProgram {
 	public static void main(String[] args){
-		System.out.print("Hello World" + "\n");
+		System.out.println("Hello World! Happy Republic Day");
 	}
 }
